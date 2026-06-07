dec49e82de15470794c801d208d9f57699ce60a9
Enchant <dmitriyslesar2005@gmail.com>
2026-06-07 22:43:55 +1000

    backfix

--- project/urls.py
+++ project/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 from app.views import UserListView, UserProfileView, RegApiView, AuthApiView
+from django.http import JsonResponse
+
+def home_view(request):
+    return JsonResponse({
+        "status":"healthy",
+        "project":"Smart Gym",
+        "version":"1.0"
+    })
 
 urlpatterns = [
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     path('list/', UserListView.as_view()),
     path('profile/', UserProfileView.as_view()),
